5fa850004ecb352aa34457848f55994767101f46
vitorLourival22 <vitorlou2204@gmail.com>
2024-08-07 21:16:58 -0300

    para concluir

--- Publication/urls.py
+++ Publication/urls.py
@@ -1,6 +1,8 @@
-from . views import index
+from . views import index , about , contact
 from django.urls import path
 
 urlpatterns = [
     path('',index,name='index'),
+    path('about/',about,name='about'),
+    path('contact/',contact,name='contact'),
 ]
--- Publication/views.py
+++ Publication/views.py
@@ -4,3 +4,5 @@ def index(request):
     return render(request, 'index.html')
 def about(request):
     return render(request, 'about.html')
+def contact(request):
+    return render(request, 'contact.html')
