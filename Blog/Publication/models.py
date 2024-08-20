494333a42f5480e8bdaf718e3fc5b0f302d4990f
vitorLourival22 <vitorLou2204@gmail.com>
2024-08-19 21:41:48 -0300

    para terminar função de pub

--- Blog/Publication/models.py
+++ Blog/Publication/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from Author.models import Author
+from django.contrib.auth.models import User
 
 class Publication(models.Model):
     Author = models.ForeignKey(Author,on_delete=models.CASCADE,verbose_name="Nome do Autor")
@@ -9,3 +10,11 @@ class Publication(models.Model):
 class Meta:
     db_table = 'publications'
 
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField(blank=True, null=True)  # Texto da postagem
+    image = models.ImageField(upload_to='posts/', blank=True, null=True)  # Imagem opcional
+    created_at = models.DateTimeField(auto_now_add=True)  # Data de criação
+
+    def __str__(self):
+        return self.text[:50]  # Retorna os primeiros 50 caracteres do texto
